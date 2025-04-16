75eeb3992118719717049f245052e197bdd5258e
SKR4VV <>
2025-04-16 22:07:50 +0200

    refactor: separate responsibility

--- src/Controller/SourceController.php
+++ src/Controller/SourceController.php
@@ -2,9 +2,9 @@
 
 namespace App\Controller;
 
-use App\Entity\Source;
-use App\Entity\UserSource;
-use Doctrine\ORM\EntityManagerInterface;
+use RuntimeException;
+use App\DTO\UserSourceDTO;
+use App\UseCase\ManageSourceUseCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -12,6 +12,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SourceController extends AbstractController
 {
+    public function __construct(private ManageSourceUseCase $manageSourceUseCase) {}
+
     #[Route('/sources', name: 'sources', methods: ['GET'])]
     public function list(): Response
     {
@@ -19,20 +21,20 @@ class SourceController extends AbstractController
         if (!$user) {
             return $this->redirectToRoute('app_login');
         }
-
-        $userSources = $user->getUserSources()->map(fn($userSource) => [
-            'id' => $userSource->getId(),
-            'url' => $userSource->getSource()->getUrl(),
-            'name' => $userSource->getName()
-        ])->toArray();
-
+    
+        $userSources = $user->getUserSources()->map(fn($userSource) => new UserSourceDTO(
+            $userSource->getId(),
+            $userSource->getSource()->getUrl(),
+            $userSource->getName()
+        ))->toArray();
+    
         return $this->render('sources/index.html.twig', [
             'sources' => $userSources
         ]);
     }
 
     #[Route('/source/add', name: 'source_add', methods: ['POST'])]
-    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    public function add(Request $request): Response
     {
         $user = $this->getUser();
         if (!$user) {
@@ -40,46 +42,14 @@ class SourceController extends AbstractController
         }
 
         $data = $request->request->all();
-        
-        if (empty($data['url']) || !self::isValidRSS($data['url'])) {
-            $this->addFlash('error', 'L\'URL fournie est invalide.');
-            return $this->redirectToRoute('sources');
+
+        try {
+            $this->manageSourceUseCase->addSource($data['url'], $data['name'], $user);
+            $this->addFlash('success', "Source ajoutée avec succès !");
+        } catch (RuntimeException $e) {
+            $this->addFlash('error', $e->getMessage());
         }
 
-        $url = htmlspecialchars(strip_tags($data['url']));
-        $name = htmlspecialchars(strip_tags($data['name']));
-
-        $existingSource = $entityManager->getRepository(Source::class)->findOneBy(['url' => $url]);
-        if ($existingSource) {
-            $this->addFlash('error', 'Cette source existe déjà.');
-            return $this->redirectToRoute('sources');
-        }
-
-        $source = new Source();
-        $source->setUrl($url);
-
-        $entityManager->persist($source);
-        $entityManager->flush();
-
-        $userSource = new UserSource();
-        $userSource->setUser($user);
-        $userSource->setSource($source);
-        $userSource->setName($name);
-
-        $entityManager->persist($userSource);
-        $entityManager->flush();
-
-        $this->addFlash('success', 'Source ajoutée avec succès !');
         return $this->redirectToRoute('sources');
-    }
-
-    private static function isValidRSS(string $url): bool
-    {
-        if (!filter_var($url, FILTER_VALIDATE_URL)) {
-            return false;
-        }
-
-        $rss = @simplexml_load_file($url);
-        return $rss && isset($rss->channel);
     }
 }
---
+++ src/UseCase/ManageSourceUseCase.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\UseCase;
+
+use RuntimeException;
+use App\Entity\Source;
+use App\Entity\UserSource;
+use App\Repository\SourceRepository;
+use App\Repository\UserSourceRepository;
+
+class ManageSourceUseCase
+{
+    public function __construct(private SourceRepository $sourceRepository, private UserSourceRepository $userSourceRepository) {}
+
+    public function addSource(string $url, string $name, $user): bool
+    {
+        if (!$this->isValidRSS($url)) {
+            throw new RuntimeException("L'URL fournie n'est pas un flux RSS valide.");
+        }
+
+        if ($this->sourceRepository->findByUrl($url)) {
+            throw new RuntimeException("Cette source existe déjà.");
+        }
+
+        $source = new Source();
+        $source->setUrl($url);
+        $this->sourceRepository->save($source);
+
+        $userSource = new UserSource();
+        $userSource->setUser($user);
+        $userSource->setSource($source);
+        $userSource->setName($name);
+
+        $this->userSourceRepository->save($userSource);
+
+        return true;
+    }
+
+    private function isValidRSS(string $url): bool
+    {
+        if (!filter_var($url, FILTER_VALIDATE_URL)) {
+            return false;
+        }
+
+        $rss = @simplexml_load_file($url);
+        return $rss && isset($rss->channel);
+    }
+}
