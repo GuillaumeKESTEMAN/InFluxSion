306ae1b805b62da47e098bc8aab48e32c566e100
Pierre Tembremande <pierre.tembremande@gmail.com>
2025-04-16 09:57:29 +0200

    renommage dans le render du LoginController

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -20,7 +20,7 @@ final class LoginController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('login.html.twig', [
+        return $this->render('login/login.html.twig', [
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -32,7 +32,7 @@ class RegistrationController extends AbstractController
 
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('articles');
+            return $this->redirectToRoute('/');
         }
 
         return $this->render('registration/register.html.twig', [
