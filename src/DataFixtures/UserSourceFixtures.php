a1b8ecd2be8a636bb877e7ee6ee5073a44a1076d
Guillaume Kesteman <guillaume.kesteman@enigma-school.com>
2025-04-16 14:12:34 +0200

    feature: add data fixtures for Article, Source, User, and UserSource entities

---
+++ src/DataFixtures/UserSourceFixtures.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Source;
+use App\Entity\User;
+use App\Entity\UserSource;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class UserSourceFixtures extends Fixture implements DependentFixtureInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 1; $i <= 3; $i++) {
+            // Récupération de la référence utilisateur
+            $user = $this->getReference("user_$i", User::class);
+
+            for ($j = 1; $j <= 2; $j++) {
+                // Récupération de la référence source
+                $source = $this->getReference("source_$j", Source::class);
+
+                $userSource = new UserSource();
+                $userSource->setName("UserSource for user$i and source$j")
+                    ->setUser($user)
+                    ->setSource($source);
+
+                $manager->persist($userSource);
+            }
+        }
+
+        $manager->flush();
+    }
+
+
+    public function getDependencies(): array
+    {
+        return [UserFixtures::class, SourceFixtures::class];
+    }
+}
