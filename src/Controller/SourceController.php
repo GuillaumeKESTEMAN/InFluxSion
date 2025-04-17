e64ee520583347b11e125b1d42eb3c84ab09ccb1
SKR4VV <188721142+SKR4VV@users.noreply.github.com>
2025-04-17 13:30:19 +0000

    feat: cron task to fetch articles (#38)
    * feature: update Article entity and fixtures for improved date handling and add saveArticles route
    
    * feature: optimize article fetching by avoiding duplicates from RSS feed
    
    * feat: cron task to fetch articles
    
    * refactor: remove unecessary method
    
    ---------
    
    Co-authored-by: Guillaume Kesteman <guillaume.kesteman@enigma-school.com>
    Co-authored-by: SKR4VV <>

--- src/Controller/SourceController.php
+++ src/Controller/SourceController.php
@@ -2,32 +2,32 @@
 
 namespace App\Controller;
 
-use RuntimeException;
-use App\Entity\Source;
 use App\DTO\UserSourceDTO;
-use App\Repository\SourceRepository;
-use App\UseCase\ManageSourceUseCase;
 use App\Repository\UserSourceRepository;
+use App\UseCase\ManageSourceUseCase;
+use RuntimeException;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SourceController extends AbstractController
 {
-    public function __construct(private ManageSourceUseCase $manageSourceUseCase) {}
+    public function __construct(private ManageSourceUseCase $manageSourceUseCase)
+    {
+    }
 
     #[Route('/sources', name: 'sources', methods: ['GET'])]
     public function list(): Response
     {
         $user = $this->getUser();
-    
+
         $userSources = $user->getUserSources()->map(fn($userSource) => new UserSourceDTO(
             $userSource->getId(),
             $userSource->getSource()->getUrl(),
             $userSource->getName()
         ))->toArray();
-    
+
         return $this->render('sources/index.html.twig', [
             'sources' => $userSources
         ]);
@@ -53,20 +53,20 @@ class SourceController extends AbstractController
     public function edit(Request $request, UserSourceRepository $userSourceRepository, int $id): Response
     {
         $userSource = $userSourceRepository->find($id);
-        
+
         if (!$userSource) {
             throw $this->createNotFoundException("Source introuvable !");
         }
-    
+
         $newName = $request->request->get('name');
-    
+
         try {
             $this->manageSourceUseCase->updateSourceName($userSource->getSource(), $newName);
             $this->addFlash('success', "Nom mis à jour avec succès !");
         } catch (\Exception $e) {
             $this->addFlash('error', $e->getMessage());
         }
-    
+
         return $this->redirectToRoute('sources');
     }
 
