5944693f667dc63fb3d902e004c00ef6534a39cf
Pierre Tembremande <pierre.tembremande@gmail.com>
2025-04-17 12:06:51 +0200

    dernier fix

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -13,7 +13,7 @@ class ArticleController extends AbstractController
     public function index(ArticleRepository $articleRepository): Response
     {
         $userSourcesIds = $this->getUser()->getUserSources()->map(fn ($userSource) => $userSource->getId())->toArray();
-        $latestArticles = $articleRepository->findLatestArticles($userSourcesIds, 10, );
+        $latestArticles = $articleRepository->findLatestArticles($userSourcesIds, 10);
 
         return $this->render('index.html.twig', [
             'articles' => $latestArticles,
