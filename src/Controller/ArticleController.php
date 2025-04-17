5aac368ae9c4cfebb02b67a79bb2ecfcb3a099e2
Pierre Tembremande <pierre.tembremande@gmail.com>
2025-04-17 11:48:38 +0200

    Les articles affichés dépendent maintenant des sources du user

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\DTO\UserSourceDTO;
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,7 +13,12 @@ class ArticleController extends AbstractController
     #[Route('/', name: 'articles', methods: ['GET'])]
     public function index(ArticleRepository $articleRepository): Response
     {
-        $latestArticles = $articleRepository->findLatestArticles(10);
+        $user = $this->getUser();
+        $userSourcesIds = $user->getUserSources()->map(function ($userSource) {
+            return $userSource->getId();
+        })->toArray();
+
+        $latestArticles = $articleRepository->findLatestArticles(10, $userSourcesIds);
 
         return $this->render('index.html.twig', [
             'articles' => $latestArticles,
--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -3,8 +3,10 @@
 namespace App\Repository;
 
 use App\Entity\Article;
+use App\Entity\UserSource;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use function Webmozart\Assert\Tests\StaticAnalysis\contains;
 
 /**
  * @extends ServiceEntityRepository<Article>
@@ -20,9 +22,11 @@ class ArticleRepository extends ServiceEntityRepository
      * @param int $limit Number of articles to get
      * @return Article[] Return array of articles
      */
-    public function findLatestArticles(int $limit = 20): array
+    public function findLatestArticles(int $limit = 20, array $userSourcesIds): array
     {
         return $this->createQueryBuilder('article')
+            ->setParameter('userSourcesId', $userSourcesIds)
+            ->where('article.source IN (:userSourcesId)')
             ->orderBy('article.date', 'DESC')
             ->setMaxResults($limit)
             ->getQuery()
